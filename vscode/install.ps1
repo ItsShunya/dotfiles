01abbdd16325a348886cace2b3e8de424ab60d4b
Shunya <luque.viictor@gmail.com>
2021-12-16 22:30:50 +0100

    Add VSCode extensions

--- vscode/install.ps1
+++ vscode/install.ps1
@@ -4,6 +4,15 @@ $inPath = "vscode/settings.json"
 $outPath = $env:APPDATA + "/Code/User/settings.json"
 
 if($env:OS -eq "Windows_NT"){
+    Write-Output ">> Copy: settings.json"
+
     $link = New-Item -ItemType SymbolicLink -Path $outPath -Target $inPath -Force
     $link | Select-Object LinkType, Target
+
+    ForEach ($ln in (Get-Content .\extensions)) {
+        Write-Output ">> Install: $ln"
+        code --install-extension "$ln"
+    }
+
+    Write-Output ">> Done."
 }
